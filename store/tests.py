d25a685ea4af0a921ff8d6fdaefc46826e43a62a
dennismonga304 <101626952+dennismonga304@users.noreply.github.com>
2022-03-15 10:35:17 +0100

    Add files via upload

---
+++ store/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+#from django.core.urlresolvers import reverse
+from .models import Commands, Categories, CustomersContact, Product
+
+
+
+# Create your tests here.
+
+
+#class IndexPageTestCase(TestCase):
+#    def test_index_page(self):
+#        response = self.client.get(reverse('index'))
+#        self.assertEqual(response.status_code, 200)
+
+
+
